f110daaea97ef9413f1306764b383ad71ba50c2a
banth <banth@DESKTOP-VTEDUKK>
2023-01-12 05:51:12 -0600

    Add project files.

---
+++ OwenDamageWPF/SwordDamage.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace OwenDamageWPF
+{
+    class SwordDamage
+    {
+        public const int BASE_DAMAGE = 3;
+        public const int FLAME_DAMAGE = 2;
+
+        public int Roll;
+        private decimal magicMultiplier = 1M;
+        private int flamingDamage = 0;
+        public int Damage;
+
+        public void CalculateDamage()
+        {
+            Damage = (int)(Roll * MagicMultiplier) + BASE_DAMAGE + FlamingDamage;
+            Debug.WriteLine($"CalculateDamage finished: {Damage} (roll: {Roll})");
+        }
+
+        public void SetMagic(bool isMagic)
+        {
+            if (isMagic)
+            {
+                MagicMultiplier = 1.75M;
+            }
+            else
+            {
+                MagicMultiplier = 1M;
+            }
+            CalculateDamage();
+            Debug.WriteLine($"SetMagic finished: {Damage} (roll: {Roll})");
+        }
+
+        public void SetFlaming(bool isFlaming)
+        {
+            CalculateDamage();
+            if (isFlaming)
+            {
+                Damage += FLAME_DAMAGE;
+            }
+            Debug.WriteLine($"SetFlaming finished: {Damage} (roll: {Roll})");
+        }
+    }
+}
